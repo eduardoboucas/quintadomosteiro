392761c7a2d41f1bea4a2fc6cf8fd72fdf8642e3
Eduardo Bouças <mail@eduardoboucas.com>
2016-04-09 17:11:55 +0100

    Add stuff

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+window.main = (function ($) {
+	var videosInteraction = false;
+	var $homepageTiles = $('.js-homepage-tile');
+	var $videos = $('.js-homepage-video');
+
+	function init() {
+		bindUiEvents();
+		autoplayFirstVideo();
+	}
+
+	function bindUiEvents() {
+		$homepageTiles.hover(function () {
+			stopAllVideos();
+			$(this).find('.js-homepage-video').get(0).play();
+		}, function () {
+			stopAllVideos();
+		});
+	}
+
+	function autoplayFirstVideo() {
+		if (!$videos.length) {
+			return;
+		}
+		
+		$videos.first().get(0).play();
+	}
+
+	function stopAllVideos() {
+		$videos.each(function (index, element) {
+			$(element).get(0).pause();
+		});
+	}
+
+	$(document).ready(init);
+})(jQuery);
